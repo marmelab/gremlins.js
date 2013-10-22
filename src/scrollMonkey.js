19928cc2fdcb20385ace2f4d240eab922ecaf691
Francois Zaninotto <fzaninotto@gmail.com>
2013-10-22 18:55:31 +0200

    Improve random scroll target and visualization

--- src/scrollMonkey.js
+++ src/scrollMonkey.js
@@ -9,8 +9,8 @@ var ScrollMonkey = (function() {
     Monkey.prototype.run = function () {
         var documentWidth = Math.max(document.body.scrollWidth, document.body.offsetWidth, document.documentElement.scrollWidth, document.documentElement.offsetWidth, document.documentElement.clientWidth),
             documentHeight = Math.max(document.body.scrollHeight, document.body.offsetHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight, document.documentElement.clientHeight),
-            scrollX = Math.floor(Math.random() * documentWidth),
-            scrollY = Math.floor(Math.random() * documentHeight);
+            scrollX = Math.floor(Math.random() * (documentWidth - document.documentElement.clientWidth)),
+            scrollY = Math.floor(Math.random() * documentHeight - document.documentElement.clientHeight);
 
         if (typeof this.config.showAction == 'function') {
             showAction(scrollX, scrollY);
@@ -22,14 +22,14 @@ var ScrollMonkey = (function() {
     var showAction = function(scrollX, scrollY) {
         var clickSignal = document.createElement('div');
         clickSignal.style.border = "3px solid red";
-        clickSignal.style.width = "40px";
-        clickSignal.style.height = "40px";
+        clickSignal.style.width = (document.documentElement.clientWidth - 20) + "px";
+        clickSignal.style.height = (document.documentElement.clientHeight - 20) + "px";
         clickSignal.style.position = "absolute";
         clickSignal.style.webkitTransition = 'opacity 1s ease-out';
         clickSignal.style.mozTransition = 'opacity 1s ease-out';
         clickSignal.style.transition = 'opacity 1s ease-out';
-        clickSignal.style.left = (scrollX - 20) + 'px';
-        clickSignal.style.top = (scrollY - 20) + 'px';
+        clickSignal.style.left = (scrollX + 10) + 'px';
+        clickSignal.style.top = (scrollY + 10) + 'px';
         var element = document.body.appendChild(clickSignal);
         setTimeout(function() {
             document.body.removeChild(element);
