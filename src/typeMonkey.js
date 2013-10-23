90fdf1db68d19f6cf001bf460ad76364f338a98e
Francois Zaninotto <fzaninotto@gmail.com>
2013-10-23 17:22:27 +0200

    Import TypeMonkey from OO version

---
+++ src/typeMonkey.js
@@ -0,0 +1,89 @@
+/*jslint browser: true*/
+
+var MonkeyTest = MonkeyTest || {};
+MonkeyTest.crew = MonkeyTest.crew || {};
+
+MonkeyTest.crew.TypeMonkey = function() {
+    "use strict";
+
+    var document = window.document,
+        documentElement = document.documentElement,
+        body = document.body;
+
+    var defaultEventTypes = ['keypress', 'keyup', 'keydown'];
+
+    var defaultShowAction = function (targetElement, x, y, key) {
+        var typeSignal = document.createElement('div');
+        typeSignal.style.border = "3px solid orange";
+        typeSignal.style['border-radius'] = '50%';
+        typeSignal.style.width = "40px";
+        typeSignal.style.height = "40px";
+        typeSignal.style['box-sizing'] = 'border-box';
+        typeSignal.style.position = "absolute";
+        typeSignal.style.webkitTransition = 'opacity 1s ease-out';
+        typeSignal.style.mozTransition = 'opacity 1s ease-out';
+        typeSignal.style.transition = 'opacity 1s ease-out';
+        typeSignal.style.left = x + 'px';
+        typeSignal.style.top = y + 'px';
+        typeSignal.style.textAlign = 'center';
+        typeSignal.style.paddingTop = '7px';
+        typeSignal.innerHTML = String.fromCharCode(key);
+
+        var element = body.appendChild(typeSignal);
+        setTimeout(function () {
+            body.removeChild(element);
+        }, 1000);
+        setTimeout(function () {
+            element.style.opacity = 0;
+        }, 50);
+    };
+
+    var getRandomElementInArray = function (arr) {
+        if (arr.length === 0) {
+            return null;
+        }
+
+        return arr[Math.floor((Math.random() * arr.length))];
+    };
+
+    var config = {
+        eventTypes: defaultEventTypes,
+        showAction: defaultShowAction
+    };
+
+    function monkey(callback) {
+        var documentWidth = Math.max(body.scrollWidth, body.offsetWidth, documentElement.scrollWidth, documentElement.offsetWidth, documentElement.clientWidth),
+            documentHeight = Math.max(body.scrollHeight, body.offsetHeight, documentElement.scrollHeight, documentElement.offsetHeight, documentElement.clientHeight),
+            keyboardEvent = document.createEvent("KeyboardEvent"),
+            initMethod = typeof keyboardEvent.initKeyboardEvent !== 'undefined' ? "initKeyboardEvent" : "initKeyEvent",
+            key = Math.floor(Math.random() * 360),
+            posX = Math.floor(Math.random() * documentElement.clientWidth),
+            posY = Math.floor(Math.random() * documentElement.clientHeight),
+            targetElement = document.elementFromPoint(posX, posY);
+
+        keyboardEvent[initMethod](getRandomElementInArray(config.eventTypes), true, true, targetElement, false, false,  false,  false,  key, 0);
+
+        targetElement.dispatchEvent(keyboardEvent);
+
+        if (typeof config.showAction === 'function') {
+            config.showAction(targetElement, posX, posY, key);
+        }
+        if (typeof callback == 'function') {
+            callback(posX, posY, key);
+        }
+    }
+
+    monkey.eventTypes = function(eventTypes) {
+        if (!arguments.length) return config.eventTypes;
+        config.eventTypes = eventTypes;
+        return monkey;
+    };
+
+    monkey.showAction = function(showAction) {
+        if (!arguments.length) return config.showAction;
+        config.showAction = showAction;
+        return monkey;
+    };
+
+    return monkey;
+};
