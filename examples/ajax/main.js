665fbdfc80cbdd7782a68595cfce7b2ea504244d
Francois Zaninotto <fzaninotto@gmail.com>
2014-09-20 23:50:25 +0200

    Simplify usage, fix send() arguments not being used

--- examples/ajax/main.js
+++ examples/ajax/main.js
@@ -5,31 +5,36 @@ require.config({
 require(['gremlins', '../../src/vendor/chance.js'], function(gremlins, Chance) {
 
     var requestEl = document.getElementById('request');
+    var i = 0;
 
     var interval = setInterval(function() {
-        requestEl.innerHTML = '<p>requesting /</p>';
+        requestEl.innerHTML += '<dt>Test ' + ++i +'</dt>';
+        requestEl.innerHTML += '<dd>';
         var req = new XMLHttpRequest();
         //0 or 1
         var rand = Math.round(Math.random());
+        var start = (new Date()).getTime();
         if (rand) {
             req.open('GET', '/', true);
+            requestEl.innerHTML += 'Requesting / ... ';
         } else {
             req.open('GET', '/404', true);
+            requestEl.innerHTML += 'Requesting /404 ... ';
         }
-        var start = (new Date()).getTime();
         req.onreadystatechange = function () {
             if (req.readyState == 4) {
                 var end = (new Date()).getTime();
                 if(req.status == 200) {
-                    requestEl.innerHTML = '<p>request succeeded</p>';
+                    requestEl.innerHTML += 'Success! ';
                 } else {
-                    requestEl.innerHTML = '<p>Error encountered while loading the page.</p>';
+                    requestEl.innerHTML += 'Error! ';
                 }
-                requestEl.innerHTML += '<p>Took '+(end - start)+' milliseconds</p>';
+                requestEl.innerHTML += '(response time: ' + (end - start) + ' ms)';
+                requestEl.innerHTML += '</dd>';
             }
         };
         req.send(null);
-    }, 1500);
+    }, 1100);
 
     var ajaxDelayer = gremlins.species.ajaxDelayer().logger(console).delayer(function () {
         var randomizer = new Chance();
@@ -37,15 +42,25 @@ require(['gremlins', '../../src/vendor/chance.js'], function(gremlins, Chance) {
         return randomizer.natural({max : 1000});
     });
 
-    gremlins
-        .createHorde()
-        .gremlin(ajaxDelayer)
-        .mogwai(function () {})
-        .after(function () {
-            clearInterval(interval);
-            requestEl.innerHTML = '<p>Finished.</p>';
-        })
-        .unleash()
-    ;
 
+    setTimeout(function() {
+        requestEl.innerHTML += '<p>Enter ajaxDelayer Gremlin</p>';
+        gremlins
+            .createHorde()
+            .gremlin(ajaxDelayer)
+            .mogwai(function () {})
+            .after(function () {
+                setTimeout(function() {
+                    requestEl.innerHTML += '<p>ajaxDelayer Gremlin is gone.</p>';
+                }, 1000);
+            })
+            .unleash()
+        ;
+
+    }, 3500);
+
+    setTimeout(function() {
+        clearInterval(interval);
+        requestEl.innerHTML += '<p>End of test.</p>';
+    }, 20000);
 });
--- src/species/ajaxDelayer.js
+++ src/species/ajaxDelayer.js
@@ -1,89 +1,83 @@
 /**
- * The ajaxDelayer gremlin delay responses from ajax request
+ * The ajax delayer gremlin adds a random delay to AJAX responses
+ *
+ * Even if called several times in a horde, this gremlin will only
+ * override the XMLHttpRequest functions once. 
  *
  *   var ajaxDelayerGremlin = gremlins.species.ajaxDelayer();
  *   horde.gremlin(ajaxDelayerGremlin);
  *
  * The ajaxDelayer gremlin can be customized as follows:
  *
- *   clickerGremlin.logger(loggerObject); // inject a logger
- *   clickerGremlin.delayer(); // inject a delay generator that return a delay in milliseconds
- *   clickerGremlin.delayAdder() //inject a function to add delay on ajax request, by default delay the execution of the send method
- *   clickerGremlin.requestReporter() //inject a function to log on ajax request open By default log the method and url requested
+ *   ajaxDelayerGremlin.delayer(); // inject a delay generator, returning a delay in milliseconds
+ *   ajaxDelayerGremlin.logger(loggerObject); // inject a logger
+ *   ajaxDelayerGremlin.randomizer(randomizerObject); // inject a randomizer
  *
  * Example usage:
  *
- *   horde.gremlin(gremlins.species.ajaxDelayer());
+ *   horde.gremlin(gremlins.species.ajaxDelayer()
+ *     .delayer(function () { return 500; }) // delay all AJAX calls by 500ms
+ *   );
  */
 define(function(require) {
     "use strict";
 
-    var RandomizerRequiredException = require('../exceptions/randomizerRequired');
     var configurable = require('../utils/configurable');
+    var RandomizerRequiredException = require('../exceptions/randomizerRequired');
 
     return function() {
-        var started = false;
 
-        var oldSend = window.XMLHttpRequest.prototype.send;
-        var oldOpen = window.XMLHttpRequest.prototype.open;
-
-        var defaultDelayAdder = function (delayer, logger) {
-
-            window.XMLHttpRequest.prototype.send = function () {
-                var delay = delayer();
-                if (typeof logger.log === 'function') {
-                    logger.log('adding delay : ', delay);
-                }
-
-                setTimeout(oldSend.bind(this), delay);
-            }
-        };
-
-        var defaultRequestReporter = function (logger) {
-            window.XMLHttpRequest.prototype.open = function (method, url) {
-                if (typeof logger.log === 'function') {
-                    logger.log('delaying ', method, url);
-                }
-
-                return oldOpen.apply(this, arguments);
-            }
-        }
+        var XHR = window.XMLHttpRequest.prototype;
 
         var defaultDelayer = function () {
             return config.randomizer.natural({ max: 1000 });
-        }
+        };
 
         /**
          * @mixin
          */
         var config = {
             delayer: defaultDelayer,
-            delayAdder: defaultDelayAdder,
-            requestReporter: defaultRequestReporter,
             logger: null,
             randomizer: null
         };
+
+        var started = false;
+        var oldSend = XHR.send;
+        var oldOpen = XHR.open;
 
         /**
          * @mixes config
          */
         var ajaxDelayerGremlin = function ajaxDelayerGremlin() {
+            if (started) {
+                return;
+            }
             if (!config.randomizer) {
                 throw new RandomizerRequiredException();
             }
 
-            if (started) {
-                return;
-            }
+            XHR.send = function () {
+                var xhrInstance = this;
+                var sendArguments = arguments;
+                var delay = config.delayer();
+                window.setTimeout(function delayedSend() {
+                    return oldSend.apply(xhrInstance, sendArguments);
+                }, delay);
+                if (config.logger && typeof config.logger.log === 'function') {
+                    config.logger.log('gremlin', 'ajaxDelayer', 'delay', this.ajaxDelayer_url, 'response by', delay, 'ms');
+                }
+            };
+            XHR.open = function (method, url) {
+                this.ajaxDelayer_url = url;
+                return oldOpen.apply(this, arguments);
+            };
             started = true;
-
-            config.requestReporter(config.logger);
-            config.delayAdder(config.delayer, config.logger);
-        }
+        };
 
         ajaxDelayerGremlin.cleanUp = function () {
-            window.XMLHttpRequest.prototype.send = oldSend;
-            window.XMLHttpRequest.prototype.open = oldOpen;
+            XHR.send = oldSend;
+            XHR.open = oldOpen;
             started = false;
         };
 
