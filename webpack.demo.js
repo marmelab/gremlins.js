373e40defce73d79a88b056cc48ee73b183f6dc2
zyhou <richard.maxime9@gmail.com>
2018-08-17 15:34:08 +0200

    linting code

--- webpack.demo.js
+++ webpack.demo.js
@@ -2,39 +2,39 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: {
-    basic: ['./examples/basic/basic.css', './examples/basic/basic.js'],
-  },
-  mode: 'development',
-  output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, 'examples/basic/dist'),
-    library: 'gremlins',
-    libraryTarget: 'umd',
-  },
-  module: {
-    rules: [
-      {
-        use: 'babel-loader',
-        test: /\.js$/,
-        include: [
-          path.resolve(__dirname, 'src'),
-          path.resolve(__dirname, 'examples/basic'),
+    entry: {
+        basic: ['./examples/basic/basic.css', './examples/basic/basic.js'],
+    },
+    mode: 'development',
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'examples/basic/dist'),
+        library: 'gremlins',
+        libraryTarget: 'umd',
+    },
+    module: {
+        rules: [
+            {
+                use: 'babel-loader',
+                test: /\.js$/,
+                include: [
+                    path.resolve(__dirname, 'src'),
+                    path.resolve(__dirname, 'examples/basic'),
+                ],
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+                include: [
+                    path.resolve(__dirname, 'src'),
+                    path.resolve(__dirname, 'examples/basic'),
+                ],
+            },
         ],
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-        include: [
-          path.resolve(__dirname, 'src'),
-          path.resolve(__dirname, 'examples/basic'),
-        ],
-      },
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'examples/basic/index.html'),
+        }),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'examples/basic/index.html'),
-    }),
-  ],
 };
