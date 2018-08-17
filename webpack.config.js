7f712b0275ad94065fe1b97dbf67df06743933e3
zyhou <richard.maxime9@gmail.com>
2018-08-17 14:32:33 +0200

    build in gremlins.min.js in dist directory

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,23 @@
 const path = require('path');
 
 module.exports = {
-  entry: {
-    index: './src/index.js',
-  },
-  mode: 'production',
-  output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, 'dist'),
-    library: 'gremlins',
-    libraryTarget: 'umd',
-  },
-  module: {
-    rules: [
-      {
-        use: 'babel-loader',
-        test: /\.js$/,
-        include: [path.resolve(__dirname, 'src')],
-      },
-    ],
-  },
+    entry: {
+        index: './src/index.js',
+    },
+    mode: 'production',
+    output: {
+        filename: 'gremlins.min.js',
+        path: path.resolve(__dirname, 'dist'),
+        library: 'gremlins',
+        libraryTarget: 'umd',
+    },
+    module: {
+        rules: [
+            {
+                use: 'babel-loader',
+                test: /\.js$/,
+                include: [path.resolve(__dirname, 'src')],
+            },
+        ],
+    },
 };
